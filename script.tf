321bc9d8093fcb88f816fe2897f4b0b89c149b9e
Ondrej Sika <ondrej@ondrejsika.com>
2021-01-13 17:27:59 +0100

    Add custom file

--- script.tf
+++ script.tf
@@ -5,5 +5,6 @@ module "script" {
 #!/bin/bash
 apt-get update
 apt-get install -y nginx
+echo "<h1>Hello from script</h1>" > /var/www/html/index.nginx-debian.html
 EOF
 }
--- simple.tf
+++ simple.tf
@@ -8,5 +8,11 @@ package_upgrade: true
 package_update: true
 packages:
   - nginx
+write_files:
+  - path: /var/www/html/index.nginx-debian.html
+    permissions: 0644
+    owner: www-data
+    content: |
+      <h1>Hello from Cloud Config</h1>
 EOF
 }
